c00db903eaf971efab0595f55acfd99ca7c48a77
yukoga <yukoga@gmail.com>
2017-09-16 22:07:31 +0900

    first commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from './app-routing.module'
+import { Angulartics2Module, Angulartics2GoogleAnalytics } from 'angulartics2';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    Angulartics2Module.forRoot([
+      Angulartics2GoogleAnalytics
+    ]),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
